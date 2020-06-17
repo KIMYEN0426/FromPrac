f987108f6ca3a052a56ea6f2ee84ba0a51f72693
KIMYEN0426 <s2018s01@e-mirim.hs.kr>
2020-06-17 14:09:20 +0900

    초기 커밋.

---
+++ FromPrac/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FromPrac
+{
+    public partial class Form1 : Form
+    {
+        public class CustomForm : Form
+        {
+            public CustomForm()
+            {
+                this.Text = "내가 만든 폼";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            IsMdiContainer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.MdiParent = this;
+            form.Show();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.ShowDialog();
+        }
+    }
+}
